d60c51b49fc33e4e166ba01f503a4169dcef6fd0
carlossprekelsen <carlos.sprekelsen@ets.ae>
2024-08-08 14:30:14 +0400

    more fixes

--- SimulationEvents.cs
+++ SimulationEvents.cs
@@ -5,30 +5,36 @@ using System.Threading.Tasks;
 public class SimulationEvents
 {
     // Simulate mouse events
-    public async Task SimulateMouseEvent(KeyCode mouseKeyCode, int? x = null, int? y = null, int? delta = null)
+    public async Task SimulateMouseEvent(MouseEvent mouseEvent)
     {
         var simulate = Simulate.Events();
-        
-        switch (mouseKeyCode)
+
+        switch (mouseEvent.Button)
         {
-            case KeyCode.MOUSE_LEFTDOWN:
-            case KeyCode.MOUSE_LEFTUP:
-            case KeyCode.MOUSE_RIGHTDOWN:
-            case KeyCode.MOUSE_RIGHTUP:
-                simulate = simulate.Click(mouseKeyCode);
+            case MouseButton.Left:
+                if (mouseEvent.Action == MouseAction.Down)
+                    simulate = simulate.LeftButtonDown();
+                else if (mouseEvent.Action == MouseAction.Up)
+                    simulate = simulate.LeftButtonUp();
                 break;
-            case KeyCode.MOUSE_MOVE:
-                if (x.HasValue && y.HasValue)
-                {
-                    simulate = simulate.MoveTo(x.Value, y.Value);
-                }
+            case MouseButton.Right:
+                if (mouseEvent.Action == MouseAction.Down)
+                    simulate = simulate.RightButtonDown();
+                else if (mouseEvent.Action == MouseAction.Up)
+                    simulate = simulate.RightButtonUp();
                 break;
-            case KeyCode.MOUSE_WHEEL:
-                if (delta.HasValue)
-                {
-                    simulate = simulate.Scroll(delta.Value);
-                }
+            case MouseButton.Middle:
+                if (mouseEvent.Action == MouseAction.Down)
+                    simulate = simulate.MiddleButtonDown();
+                else if (mouseEvent.Action == MouseAction.Up)
+                    simulate = simulate.MiddleButtonUp();
                 break;
+            // Add more cases as necessary
+        }
+
+        if (mouseEvent.X.HasValue && mouseEvent.Y.HasValue)
+        {
+            simulate.MoveMouseToPositionOnVirtualDesktop(mouseEvent.X.Value, mouseEvent.Y.Value);
         }
 
         await simulate.Invoke();
@@ -39,4 +45,29 @@ public class SimulationEvents
     {
         await Simulate.Events().Click(keyCode).Invoke();
     }
+}
+
+// Define MouseEvent struct or class as needed
+public struct MouseEvent
+{
+    public MouseButton Button;
+    public MouseAction Action;
+    public int? X;
+    public int? Y;
+    public int? Delta;  // For scroll events if necessary
+}
+
+public enum MouseButton
+{
+    Left,
+    Right,
+    Middle
+}
+
+public enum MouseAction
+{
+    Down,
+    Up,
+    Move,
+    Scroll
 }
